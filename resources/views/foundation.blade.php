f6f5a1e7fb5a92f2590266e800a3fe884ab760ad
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-04-24 22:59:48 +0200

    improve website content
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- resources/views/foundation.blade.php
+++ resources/views/foundation.blade.php
@@ -8,47 +8,77 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dicta excepturi nostrum
-                        obcaecati pariatur placeat quae quos repellat sunt tenetur? Adipisci beatae ex inventore ipsa
-                        modi non quas, repellendus unde?
-                    </div>
-                    <div>Amet autem, fugit itaque modi odio officia quam saepe sequi? Assumenda consequuntur cum dolor
-                        dolorem dolores illum iure libero molestiae nam nisi numquam omnis optio qui rem sed, sequi
-                        voluptas?
-                    </div>
-                    <div>Aut eum exercitationem molestiae, natus possimus quaerat sed ut! Accusantium asperiores autem
-                        distinctio eius in inventore, labore laudantium nisi placeat quidem quod repellendus similique
-                        sint sunt temporibus ut voluptas voluptatibus.
-                    </div>
-                    <div>Ad culpa cumque dolores omnis quasi, qui quidem ratione reprehenderit sit! Dolore fugit id
-                        inventore magnam nemo nesciunt quaerat repellendus sint vel! Accusantium asperiores ipsam
-                        laborum perspiciatis totam ullam vero?
-                    </div>
-                    <div>Accusantium ad blanditiis culpa deleniti quo! Blanditiis earum exercitationem harum ipsam ipsum
-                        iste magnam, necessitatibus odio perferendis praesentium provident, qui velit. At distinctio
-                        earum est explicabo harum ipsam obcaecati vitae!
-                    </div>
-                    <div>Ad amet, animi aperiam consequatur consequuntur ducimus expedita fuga fugit impedit iusto
-                        laboriosam necessitatibus neque nihil nostrum omnis pariatur, rem sint ut! Culpa, debitis
-                        excepturi fuga iste non voluptatem voluptates.
-                    </div>
-                    <div>Ab ea eligendi est, eum explicabo fugit nihil omnis provident quam reprehenderit sed vero,
-                        voluptatem! Ab distinctio ea ex ipsa, natus, optio porro praesentium quidem quo sint temporibus
-                        velit voluptatem.
-                    </div>
-                    <div>Adipisci amet architecto aspernatur at blanditiis consequatur illo libero, maxime neque qui
-                        ratione temporibus. Aliquam, corporis cum, deleniti deserunt dolorem id illum, odio quis
-                        quisquam recusandae sequi sit voluptate voluptatibus.
-                    </div>
-                    <div>Ab aut commodi culpa cumque cupiditate dolores esse excepturi facere fuga maiores molestias
-                        nisi nulla odio odit officia quaerat quibusdam quidem, rem repellat sapiente sequi sint veniam
-                        voluptas voluptatem voluptates.
-                    </div>
-                    <div>Atque deserunt doloribus ipsum magni nihil possimus quod. Ad architecto asperiores aspernatur
-                        aut doloribus ducimus ex itaque maiores, mollitia neque quidem repellendus soluta velit? Dolorem
-                        ex expedita molestias obcaecati ut!
-                    </div>
+                <div class="p-6 bg-white border-b border-gray-200 prose">
+                    <h1>Our foundation</h1>
+
+                    <h2>About us</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur blanditiis consectetur
+                        eligendi hic in inventore laboriosam molestiae molestias perferendis perspiciatis, quam quasi,
+                        quod sequi similique sunt suscipit veniam voluptatum!
+                    </p>
+                    <p>Commodi distinctio doloribus esse expedita fugit harum modi, obcaecati voluptatem. Accusamus,
+                        aut culpa cumque eaque eos iste itaque maiores neque optio quam quos repellendus reprehenderit
+                        saepe tempore tenetur vitae voluptate?
+                    </p>
+                    <p>Cupiditate eligendi iure maiores nam praesentium quod temporibus? Amet animi atque blanditiis
+                        eum harum, iusto minus, nostrum praesentium quasi saepe sapiente, soluta tempore unde!
+                        Aspernatur delectus deleniti eum recusandae soluta!
+                    </p>
+
+                    <h2>History</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam maiores officiis optio
+                        quae
+                        quas voluptatibus. Accusamus alias amet culpa cupiditate eius exercitationem, laudantium maiores
+                        quam similique tempora ullam, velit.
+                    </p>
+                    <p>Assumenda deserunt eius et facilis incidunt inventore laudantium, maxime minima nam non quas quia
+                        quibusdam reprehenderit rerum tempora? Aut dicta, exercitationem fuga fugiat iste provident
+                        soluta
+                        suscipit temporibus! Sequi, soluta.
+                    </p>
+                    <p>Assumenda beatae culpa delectus dolor dolore dolores dolorum, ducimus eos et eveniet hic
+                        inventore
+                        ipsa minima non numquam, odio quae quam quas quasi ratione, repellat sequi sit veritatis vero
+                        voluptatem.
+                    </p>
+                    <p>A adipisci amet aperiam commodi corporis culpa cum eos eum fuga inventore ipsa ipsam iure magnam
+                        maiores, molestiae omnis optio pariatur perspiciatis quo rem sapiente sequi veniam voluptas?
+                        Alias,
+                        nobis.
+                    </p>
+                    <p>Architecto beatae doloribus illo labore non omnis perferendis possimus provident, voluptates
+                        voluptatum. Animi autem blanditiis, debitis dolor eum excepturi illo ipsum itaque laboriosam,
+                        minus
+                        odit pariatur provident ratione repellendus sunt.
+                    </p>
+                    <p>Architecto assumenda cum inventore pariatur possimus, ratione ut. Aliquam aspernatur autem
+                        debitis,
+                        dolores doloribus dolorum ducimus eaque eos magnam nisi omnis repellat reprehenderit soluta ut
+                        veniam vero vitae voluptatem voluptates.
+                    </p>
+                    <p>Animi, autem culpa debitis delectus deleniti deserunt dolorem ducimus ea facilis minima
+                        molestiae,
+                        natus necessitatibus nihil nulla obcaecati odio placeat quis sit sunt veniam. Amet dolor
+                        inventore
+                        iste suscipit totam.
+                    </p>
+                    <p>Alias aspernatur aut cumque earum expedita nemo non possimus tenetur. Architecto atque autem
+                        dolorum eius modi odit porro similique, vel velit vitae? Deleniti earum ex itaque necessitatibus
+                        quidem saepe soluta?
+                    </p>
+                    <p>Animi, consectetur distinctio eaque eius et ex inventore ipsam magnam nobis non omnis
+                        praesentium,
+                        quae quaerat repellat sint soluta velit, veritatis. Accusantium adipisci dignissimos eligendi,
+                        eos
+                        maiores minus neque possimus.
+                    </p>
+                    <p>Accusamus adipisci at eos, error esse est, et eveniet excepturi facere illum impedit ipsa
+                        laboriosam nam natus necessitatibus neque omnis porro provident quae quisquam ratione
+                        reprehenderit,
+                        repudiandae tenetur voluptas voluptatem?
+                    </p>
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,55 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Home content
+                <div class="p-6 bg-white border-b border-gray-200 prose">
+                    <h1>Home</h1>
+                    <strong>Welcome to our foundation!</strong>
+
+                    <p>Our mission is lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore inventore minima molestias non quibusdam sequi tempore. Accusantium autem commodi, consectetur esse fuga, fugiat maiores, mollitia nesciunt nostrum quibusdam quidem voluptatibus.</p>
+
+                    <h2>Mission</h2>
+
+                    <img src="/static/mission.jpg" alt="Our mission">
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam deserunt laboriosam modi
+                        nam non odit ratione rerum ut vitae! Accusantium dolorum error impedit minus neque nobis officia
+                        quae, voluptatem?
+                    </p>
+                    <p>Consequatur, cumque deleniti dolor earum laborum, minus mollitia nam nemo reiciendis tempora
+                        temporibus tenetur unde! A alias amet architecto consequatur, inventore nam neque odio
+                        perferendis perspiciatis, praesentium quaerat, quisquam unde!
+                    </p>
+                    <p>Aut autem distinctio ducimus earum facere fugiat fugit impedit iste minima molestiae possimus,
+                        quis, repellendus voluptatum. Aperiam aspernatur dolorem expedita magni modi omnis praesentium
+                        repellat ut? Aliquam optio praesentium quo.
+                    </p>
+                    <p>Dolorum facere mollitia, nemo nihil non quisquam rem saepe. Ab corporis dolore neque quibusdam
+                        velit. Ab aliquid incidunt magni, minima mollitia obcaecati porro quae velit. Earum et
+                        repudiandae saepe vero.
+                    </p>
+
+                    <h2>Our work</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam deserunt laboriosam modi
+                        nam non odit ratione rerum ut vitae! Accusantium dolorum error impedit minus neque nobis officia
+                        quae, voluptatem?
+                    </p>
+                    <p>Consequatur, cumque deleniti dolor earum laborum, minus mollitia nam nemo reiciendis tempora
+                        temporibus tenetur unde! A alias amet architecto consequatur, inventore nam neque odio
+                        perferendis perspiciatis, praesentium quaerat, quisquam unde!
+                    </p>
+                    <p>Aut autem distinctio ducimus earum facere fugiat fugit impedit iste minima molestiae possimus,
+                        quis, repellendus voluptatum. Aperiam aspernatur dolorem expedita magni modi omnis praesentium
+                        repellat ut? Aliquam optio praesentium quo.
+                    </p>
+                    <p>Dolorum facere mollitia, nemo nihil non quisquam rem saepe. Ab corporis dolore neque quibusdam
+                        velit. Ab aliquid incidunt magni, minima mollitia obcaecati porro quae velit. Earum et
+                        repudiandae saepe vero.
+                    </p>
+
+                    <h2>Meet the team</h2>
+                    Read more about our foundation <a href="{{ route('foundation') }}">here</a> or check out some photos about our events in our
+                    <a href="{{ route('photos') }}">image gallery</a>.
                 </div>
             </div>
         </div>
