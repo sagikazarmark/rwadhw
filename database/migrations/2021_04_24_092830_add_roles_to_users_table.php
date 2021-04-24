ec7eabaa5313d5b9f3d8f7b2d5ff51c09a9d5bad
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-04-24 13:55:03 +0200

    fix default user role
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- database/migrations/2021_04_24_092830_add_roles_to_users_table.php
+++ database/migrations/2021_04_24_092830_add_roles_to_users_table.php
@@ -14,7 +14,7 @@ class AddRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer("role")->after('password')->default(2);
+            $table->integer("role")->after('password')->default(1);
         });
     }
 
