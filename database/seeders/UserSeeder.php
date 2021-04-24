7e75a6cd2c486e11ea4840f01225f2733721ee9d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-04-24 11:16:50 +0200

    add user seeder
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Aladár',
+            'email' => 'admin@rwadhw.sagikazarmark.hu',
+            'email_verified_at' => now(),
+            'password' => Hash::make('symfonyftw123**'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'User Ubul',
+            'email' => 'user@rwadhw.sagikazarmark.hu',
+            'email_verified_at' => now(),
+            'password' => Hash::make('symfonyftw123**'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Guest Gábor',
+            'email' => 'guest@rwadhw.sagikazarmark.hu',
+            'email_verified_at' => now(),
+            'password' => Hash::make('symfonyftw123**'),
+        ]);
+    }
+}
